15599402b869d8df8d11699a77b47ee6a30d50d9
vishu-tyagi <vishweshwartyagiv@gmail.com>
2023-01-17 09:53:10 +0000

    add package

---
+++ src/ecom-sales/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="ecom_sales",
+    version="0.0.1",
+    description="Package for E-commerce Dashboard",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9"
+)
