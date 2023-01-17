ab90166da077fbfe955362b6dd08ff86013ce11c
Vishu Tyagi <70053919+vishu-tyagi@users.noreply.github.com>
2023-01-17 08:30:39 -0500

    Add utility to load into postgres (#4)

--- src/ecom-sales/ecom_sales/config.py
+++ src/ecom-sales/ecom_sales/config.py
@@ -5,5 +5,7 @@ class EcomConfig():
 
     # Files to download from S3 bucket
     S3_BUCKET_RELEVANT_FILES = {
-        "ecom_sales.csv": "ecom_sales.csv"
+        "ecom_sales": {
+            "ecom_sales.csv": "ecom_sales.csv"
+        }
     }
