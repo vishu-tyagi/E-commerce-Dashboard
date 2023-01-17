32ca2cd8a3607ccf2253f178f9544035782f3523
vishu-tyagi <vishweshwartyagiv@gmail.com>
2023-01-17 10:09:42 +0000

    update bucket name

--- src/ecom-sales/ecom_sales/config.py
+++ src/ecom-sales/ecom_sales/config.py
@@ -1,9 +1,9 @@
 class EcomConfig():
     # S3 bucket for fetching raw data
-    S3_BUCKET = "raw-data"
+    S3_BUCKET = "raw-data-vt"
     CURRENT_PATH = None   # will be set to working directory by os.getcwd()
 
     # Files to download from S3 bucket
     S3_BUCKET_RELEVANT_FILES = {
-        "ecom_sales": "ecom_sales_data.csv"
+        "ecom_sales.csv": "ecom_sales.csv"
     }
